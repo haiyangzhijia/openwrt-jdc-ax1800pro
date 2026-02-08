a5a75c92e5d7ebcca4fbec2d30e125f8d9ed2dbb
Ubuntu <azureuser@Moltbot.ocicktlkph3ezm3zohg0rg0leb.phxx.internal.cloudapp.net>
2026-02-08 20:39:20 +0000

    Add Siriling luci-app-modem support

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -2,6 +2,9 @@
 # DIY Part 1: Execute before updating feeds
 
 echo "DIY Part 1: Add custom feeds"
+# Add Siriling's feed for 5G Modem Support
+echo 'src-git siriling https://github.com/Siriling/5G-Modem-Support.git' >> feeds.conf.default
+
 # Passwall and other feeds are already in feeds.conf.default
 
 # Add any extra logic here if needed
